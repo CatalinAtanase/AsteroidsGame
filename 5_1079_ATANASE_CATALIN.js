be2aba6c21be6606ca3c6b9c6d1850e5b7643f85
catalinatanase <atanasecatalin20@gmail.com>
2022-01-02 20:54:57 +0200

    :

--- 5_1079_ATANASE_CATALIN.js
+++ 5_1079_ATANASE_CATALIN.js
@@ -2,23 +2,19 @@ const gameContainer = document.querySelector(".gameContainer");
 const entryContainer = document.querySelector(".entry-container");
 const startGameBtn = document.querySelector("#startGameBtn");
 const playerName = document.querySelector("#playerName");
+const playAgain = document.querySelector("#playAgain");
 const scoresContainer = document.querySelector(".scores");
 
-let scores = JSON.parse(localStorage.getItem("scores"));
+let scores;
+
+const ASTEROID_MAX_HP = 4;
+const FRAMES = 1000 / 60;
 
 let canvas, canvasContext, canvasWidth, canvasHeight;
 let asteroids = [];
 let asteroidYSpeed = 0,
   asteroidXSpeed = 0;
 let asteroidWidth, asteroidHeight, asteroidRadius;
-/*     
-             COORDONATE NAVA
-             3 score:
-             dreapta (400,550) (shipLeftX,shipLeftY)
-             stanga(450, 550) (shipRightX, shipRightY)
-             sus(425, 500)    (shipTopX, yNava Sus)
-     */
-
 let shipLeftX = 400,
   shipLeftY = 550;
 let shipRightX = 450,
@@ -37,7 +33,6 @@ let score = 0;
 let isMovingRocket;
 
 let isDone;
-const FRAMES = 1000 / 60;
 
 const drawAsteroid = (asteroid) => {
   canvasContext.beginPath();
@@ -55,13 +50,7 @@ const drawAsteroid = (asteroid) => {
   }
 
   if (asteroid.hp > 0) {
-    canvasContext.arc(
-      asteroid.x,
-      asteroid.y,
-      asteroid.hp * asteroid.radius,
-      0,
-      2 * Math.PI
-    );
+    canvasContext.arc(asteroid.x, asteroid.y, asteroid.radius, 0, 2 * Math.PI);
     canvasContext.fill();
     canvasContext.beginPath();
     canvasContext.fillStyle = "black";
@@ -90,6 +79,8 @@ const checkRocketColision = (asteroid) => {
   if (distanta < asteroid.radius && asteroid.hp > 0 && isMovingRocket) {
     placeRocket();
     asteroid.hp--;
+    asteroid.radius = asteroid.hp * asteroidRadius;
+    ASTEROID_FUNCTIONS.setAsteroidSpeed(asteroid)
     if (asteroid.hp < 1) {
       asteroids = asteroids.filter((a) => a.id !== asteroid.id);
     }
@@ -102,24 +93,20 @@ const checkRocketColision = (asteroid) => {
 };
 
 const checkShipCollision = (asteroid) => {
-  //distanta dintre asteroid si punctul de sus
   let topDistance = Math.sqrt(
     (shipTopX - asteroid.x) * (shipTopX - asteroid.x) +
       (shipTopY - asteroid.y) * (shipTopY - asteroid.y)
   );
 
-  //distanta dintre asteroid si punctul din stanga
   let leftDistance = Math.sqrt(
     (shipLeftX - asteroid.x) * (shipLeftX - asteroid.x) +
       (shipLeftY - asteroid.y) * (shipLeftY - asteroid.y)
   );
 
-  //distanta dintre asteroid si punctul din dreapta
   let rightDistance = Math.sqrt(
     (shipRightX - asteroid.x) * (shipRightX - asteroid.x) +
       (shipRightY - asteroid.y) * (shipRightY - asteroid.y)
   );
-
   if (
     (rightDistance < asteroid.radius ||
       leftDistance < asteroid.radius ||
@@ -141,15 +128,20 @@ const shipCollision = () => {
 };
 
 const moveAsteroid = (asteroid) => {
-  if (asteroid.y + asteroidYSpeed >= canvasHeight || asteroid.y < 0) {
-    asteroidYSpeed = -asteroidYSpeed;
+  if (asteroid.y + asteroid.ySpeed >= canvasHeight || asteroid.y < 0) {
+    asteroid.ySpeed = -asteroid.ySpeed;
   }
-  asteroid.y += asteroidYSpeed;
-  if (asteroid.x + asteroidXSpeed >= canvasWidth || asteroid.x < 0) {
-    asteroidXSpeed = -asteroidXSpeed;
+  asteroid.y += asteroid.ySpeed;
+  if (asteroid.x + asteroid.xSpeed >= canvasWidth || asteroid.x < 0) {
+    asteroid.xSpeed = -asteroid.xSpeed;
   }
-  asteroid.x += asteroidXSpeed;
+  asteroid.x += asteroid.xSpeed;
   shipCollision();
+};
+
+const setAsteroidSpeed = (asteroid) => {
+  asteroid.xSpeed = asteroidXSpeed * (ASTEROID_MAX_HP - asteroid.hp + 1);
+  asteroid.ySpeed = asteroidYSpeed * (ASTEROID_MAX_HP - asteroid.hp + 1);
 };
 
 const ASTEROID_FUNCTIONS = {
@@ -157,6 +149,7 @@ const ASTEROID_FUNCTIONS = {
   checkRocketColision,
   checkShipCollision,
   moveAsteroid,
+  setAsteroidSpeed,
 };
 
 const drawShip = () => {
@@ -195,7 +188,7 @@ const drawScore = () => {
 };
 
 const checkEndGame = () => {
-  if (shipHP === 0) {
+  if (shipHP === 0 || asteroids.length === 0) {
     canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
     canvasContext.beginPath();
     canvasContext.fillStyle = "white";
@@ -247,34 +240,29 @@ const onFrameMove = () => {
 };
 //Miscare nava
 const moveShip = (e) => {
-  //stanga
   if (e.keyCode === 37 && shipLeftX >= 0) {
     shipRightX -= shipSpeed;
     shipLeftX -= shipSpeed;
     shipTopX -= shipSpeed;
   }
-  //dreapta
   if (e.keyCode === 39 && shipRightX <= canvasWidth) {
     shipRightX += shipSpeed;
     shipLeftX += shipSpeed;
     shipTopX += shipSpeed;
   }
-  //sus
   if (e.keyCode === 38 && shipTopY >= 0) {
     shipRightY -= shipSpeed;
     shipLeftY -= shipSpeed;
     shipTopY -= shipSpeed;
   }
-  //jos
   if (
     e.keyCode === 40 &&
-    shipLeftY <= 550 /*Nu las nava mai jos decat pozitia initiala*/
+    shipLeftY <= 600
   ) {
     shipRightY += shipSpeed;
     shipLeftY += shipSpeed;
     shipTopY += shipSpeed;
   }
-  //X->lansez rachete
   if (e.keyCode === 88) {
     if (!isMovingRocket) {
       isMovingRocket = setInterval(() => {
@@ -289,40 +277,63 @@ const moveShip = (e) => {
 };
 
 const rocketAsteroidCollision = () => {
+  if(asteroids.length === 0) {
+    canvasContext.fillText(`Game over! Scor: ${score}`, 150, 300);
+  }
   asteroids.forEach(ASTEROID_FUNCTIONS.checkRocketColision);
 };
 
-const app = () => {
+const showScores = () => {
+  scoresContainer.textContent = "";
+  scores = JSON.parse(localStorage.getItem("scores"));
   if (scores) {
-    scores.sort((a, b) => b.score - a.score)
+    scores.sort((a, b) => b.score - a.score);
     scores.forEach((value) => {
       const p = document.createElement("p");
       p.textContent = `${value.name} - ${value.score}`;
       scoresContainer.appendChild(p);
     });
   } else {
-    scores = []
+    scores = [];
   }
+};
+
+const setup = () => {
+  score = 0;
+  shipHP = 3;
+  (shipLeftX = 400), (shipLeftY = 550);
+  (shipRightX = 450), (shipRightY = 550);
+  (shipTopX = 425), (shipTopY = 500);
+  shipSpeed = 5;
+  shipHP = 3;
+  isShipImmune = false;
+  (rocketX = 425), (rocketY = 500);
+  asteroids = [];
+};
+
+const app = () => {
+  showScores();
+  setup();
   canvas = document.querySelector("#gameCanvas");
   canvasWidth = canvas.width;
   canvasHeight = canvas.height;
   canvasContext = canvas.getContext("2d");
 
-  const counter = 5;
+  const counter = 8;
   asteroidWidth = canvasWidth / counter;
   asteroidHeight = canvasHeight / counter;
   asteroidRadius = canvasHeight / 40;
   asteroidYSpeed = Math.random() * 2;
-  asteroidXSpeed = Math.random() * 2;
+  asteroidXSpeed = Math.random();
 
   let k = 1;
   for (let i = 0; i < counter; i++) {
-    if (k <= 4) {
+    if (k <= ASTEROID_MAX_HP) {
       asteroids.push({
         id: i,
         x: k * asteroidWidth + Math.random() * 150,
-        y: k * asteroidHeight + Math.random() * 150,
-        radius: asteroidRadius,
+        y: k * asteroidHeight + Math.random() * 20,
+        radius: asteroidRadius * k,
         hp: k,
       });
       k++;
@@ -330,17 +341,25 @@ const app = () => {
       k = 1;
     }
   }
+  asteroids.forEach(ASTEROID_FUNCTIONS.setAsteroidSpeed)
   drawCanvas();
   setInterval(onFrameMove, FRAMES);
   document.addEventListener("keydown", moveShip);
   setInterval(rocketAsteroidCollision, FRAMES);
 };
 
-document.addEventListener("DOMContentLoaded", app);
+document.addEventListener("DOMContentLoaded", showScores);
 
 startGameBtn.addEventListener("click", () => {
   if (playerName.value.trim() != "") {
     gameContainer.style.display = "flex";
     entryContainer.style.display = "none";
+    app();
   }
+});
+
+playAgain.addEventListener("click", (e) => {
+  gameContainer.style.display = "none";
+  entryContainer.style.display = "flex";
+  showScores();
 });
